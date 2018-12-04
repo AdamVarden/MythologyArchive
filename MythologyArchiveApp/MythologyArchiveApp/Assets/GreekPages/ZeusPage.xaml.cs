8b5206bab45024a27f51ce7efc0f962eab28d536
ADAM VARDEN <G00359605@gmit.ie>
2018-12-04 20:05:39 +0000

    Added Hades,Poseidon and Zeus to GreekPages. Need to add content next

---
+++ MythologyArchiveApp/MythologyArchiveApp/Assets/GreekPages/ZeusPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MythologyArchiveApp.Assets.GreekPages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ZeusPage : ContentPage
+	{
+		public ZeusPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
